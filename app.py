2789de96fb4dcf9be7ed28a42dea09697b1effd7
Md Taukir Alam <taukir.pcet@gmail.com>
2025-05-02 23:22:58 +0530

    api changes

--- app.py
+++ app.py
@@ -1,32 +1,32 @@
-# from flask import Flask ,request
-# app = Flask(__name__)
-
-# store = [
-#     {
-#         "name":"My Store",
-#         "items":[
-#             {
-#                 "name":"Chair",
-#                 "price":15.99
-#             }
-#         ]
-#     }
-# ]
-
-# @app.get("/store")
-# def get_store():
-#     return {"store":store}
-
-# @app.post("/store")
-# def create_store():
-#     request_data = request.get_json()
-#     new_store = {"name":request_data['name'],"items":[]}
-#     store.append(new_store)
-#     return new_store , 201
 from flask import Flask, request, jsonify
 import redis
 
 app = Flask(__name__)
+
+store = [
+    {
+        "name":"My Store",
+        "items":[
+            {
+                "name":"Chair",
+                "price":15.99
+            }
+        ]
+    }
+]
+
+@app.get("/store")
+def get_store():
+    print("dscjsdcsdcn")
+    return {"store":store}
+
+@app.post("/store")
+def create_store():
+    request_data = request.get_json()
+    new_store = {"name":request_data['name'],"items":[]}
+    store.append(new_store)
+    return new_store , 201
+
 
 # Connect to Redis
 redis_client = redis.Redis(host='localhost', port=6379, db=0, decode_responses=True)
